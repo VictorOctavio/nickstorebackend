48ee33bbe01fcd36c85f66db6e9cf41c74e5f668
Octavio <gaunavictoroctavio@gmail.com>
2022-06-08 09:06:26 -0300

    2 make it better

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const app = require('./app');
 
 mongoose.connect(process.env.mongoose, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
-.then(() => {
-    console.log('connecting mongoose successfy');
+.then((db) => {
+
+    console.log('connecting mongoose successfy', db.connection.name);
 
     app.listen(app.get('port'), () => {
         console.log(`server on port ${app.get('port')}`);
