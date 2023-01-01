10d3f4e8b64ca4620f17452300f8233808fc9deb
Octavio <gaunavictoroctavio@gmail.com>
2023-01-01 08:13:42 -0300

    firs-commit-nickstore-backend

--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ const app = require('./app');
 
 mongoose.connect(process.env.mongoose, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
 .then((db) => {
-
     console.log('connecting mongoose successfy', db.connection.name);
 
     app.listen(app.get('port'), () => {
         console.log(`server on port ${app.get('port')}`);
     });
-
 })
 .catch(err => console.log(err));
